c9ca92c5f017016ea4df9dee10d64418b0b59584
Martin Frederik SCHMITZ <schmitzmf@gis.a-star.edu.sg>
2021-12-02 16:54:13 +0800

    add to main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os.path
 import load_assembly_graph_from_csv as fromcsv
 
+def main_routine():
 
-print("Hello World")
-graph_path = os.path.join("ecoli", "graph_1.csv")
-graph = fromcsv.load_graph(graph_path)
-print(graph)
+    graph_path = os.path.join("ecoli", "graph_1.csv")
+    graph = fromcsv.load_graph(graph_path)
+    print(graph)
+
+if __name__ == '__main__':
+    main_routine()
+
